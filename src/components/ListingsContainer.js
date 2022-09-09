28413f02e6ef852fb49170c3b7a8725064d8ca44
Ben <berkhart@rocketmail.com>
2022-09-09 00:18:12 -0500

    Completed practice

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,26 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useState, useEffect } from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ listing, onDeleteHandler, searchTerm }) {
+
+
+  const [filterListings, setFilterListing] = useState([])
+
+  useEffect(() => {
+    setFilterListing(listing.filter((list) => list.description.toLowerCase().includes(searchTerm.toLowerCase())))
+  }, [searchTerm, listing])
+
+  const cards = (filterListings.length ? filterListings : listing).map((list) => (
+    <ListingCard 
+      listing={list} 
+      onDeleteHandler={onDeleteHandler} 
+      />
+  ))
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {cards}
       </ul>
     </main>
   );
